d674bbc03df2f50689b4a26992f36a0e2294411c
unknown <anusondd@gmail.com>
2018-06-20 14:18:26 +0700

    s2

--- src/app/page/index/index.component.ts
+++ src/app/page/index/index.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
-
+  list = [];
+  list01 = [];
+  list02 = [];
   async ngAfterViewInit() {
     await this.loadScript('assets/engine1/jquery.js');
     await this.loadScript('assets/js/owl.carousel.js');
@@ -21,7 +23,19 @@ export class IndexComponent implements OnInit {
     })
   }
 
-  constructor() { }
+
+
+  constructor() {
+    for (let i = 0; i < 6; i++) {
+      this.list.push({ num: i });
+    }
+    for (let s = 0; s < 6; s++) {
+      this.list01.push({ num: s });
+    }
+    for (let p = 0; p < 4; p++) {
+      this.list02.push({ num: p });
+    }
+  }
 
   ngOnInit() {
   }
